860b404e917ff6798d3856dc7d3e8d2510262228
Marty Nelson <marty.nelson@yahoo.com>
2011-12-16 15:16:42 -0800

    change to commander, added package.json

--- console-prompt.js
+++ console-prompt.js
@@ -1,3 +1,4 @@
+var program = require('commander')
 module.exports = function(prompts, cb){
   
   var addends = [];
@@ -8,26 +9,10 @@ module.exports = function(prompts, cb){
       return cb(addends)
     }
     
-    getNumber(inputPrompt, function(number){
+    program.prompt(inputPrompt, Number, function(number){
       addends.push(number)
       input()  
     })
        
   })()
 }
-
-function getNumber(str, fn){
-  getValue(str, function parseNumber(val){
-    val = Number(val);
-    if (isNaN(val)) return getValue(str + '(must be a number) ', parseNumber);
-    fn(val);
-  });
-};
-
-function getValue(str, cb){
-  process.stdout.write(str);
-  process.stdin.setEncoding('utf8');
-  process.stdin.once('data', function(val){
-    cb(val.trim());
-  }).resume()
-};
